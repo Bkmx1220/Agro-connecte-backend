f8cdd4adabdb2b6f5ef54111f5116ecb1ca37648
Bkmx1220 <yahbkm011@gmail.com>
2026-02-25 18:56:19 +0000

    postgres config

--- agro_platform/settings.py
+++ agro_platform/settings.py
@@ -81,7 +81,9 @@ WSGI_APPLICATION = "agro_platform.wsgi.application"
 # ========================
 DATABASES = {
     'default': dj_database_url.parse(
-        config("DATABASE_URL")
+        config("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
