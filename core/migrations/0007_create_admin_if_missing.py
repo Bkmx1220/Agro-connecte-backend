b1e7c7be6b2642c7a382fd0fc4cd3bed199956cb
Bkmx1220 <yahbkm011@gmail.com>
2026-09-11 13:58:47 +0000

    Ensure production admin exists

---
+++ core/migrations/0007_create_admin_if_missing.py
@@ -0,0 +1,40 @@
+from django.db import migrations
+import os
+
+
+def create_admin_if_missing(apps, schema_editor):
+    User = apps.get_model("core", "User")
+
+    username = os.environ.get("ADMIN_USERNAME")
+    email = os.environ.get("ADMIN_EMAIL")
+    password = os.environ.get("ADMIN_PASSWORD")
+
+    if not username or not email or not password:
+        return
+
+    if User.objects.filter(username=username).exists():
+        return
+
+    user = User(
+        username=username,
+        email=email,
+        role="admin",
+        is_staff=True,
+        is_superuser=True,
+        is_verified=True,
+        is_active=True,
+    )
+
+    user.set_password(password)
+    user.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0006_create_admin"),
+    ]
+
+    operations = [
+        migrations.RunPython(create_admin_if_missing),
+    ]
